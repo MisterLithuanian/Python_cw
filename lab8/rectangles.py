152a107628648c59b831de9db72ef98f0b070b41
JakubLit <kuba.litwicki.tel@gmail.com>
2026-01-27 23:42:41 +0100

    2

--- lab8/rectangles.py
+++ lab8/rectangles.py
@@ -1,45 +1,7 @@
-from dataclasses import dataclass
-@dataclass(frozen=True)
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-    def __repr__(self):
-        return f"Point({self.x}, {self.y})"
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
-
-    def __mul__(self, other):
-        return self.x * other.x + self.y * other.y
-
-    def cross(self, other):
-        return self.x * other.y - self.y * other.x
-
-    def length(self):
-        return (self.x**2 + self.y**2)**0.5
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-
+from points import Point
 
 
 class Rectangle:
-
     def __init__(self, x1, y1, x2, y2):
         self.pt1 = Point(x1, y1)
         self.pt2 = Point(x2, y2)
@@ -56,75 +18,66 @@ class Rectangle:
     def __ne__(self, other):
         return not self == other
 
-    def center(self):
-        return Point(
-            (self.pt1.x+self.pt2.x)/2,
-            (self.pt1.y+self.pt2.y)/2
-        )
 
-    def area(self):
-        return abs(self.pt2.x-self.pt1.x)*abs(self.pt2.y-self.pt1.y)
-
-    def move(self, x, y):
-        self.pt1 = Point(self.pt1.x + x, self.pt1.y + y)
-        self.pt2 = Point(self.pt2.x + x, self.pt2.y + y)
-        return self
     @classmethod
     def from_points(cls, points):
         p1, p2 = points
-        x1, y1 = p1
-        x2, y2 = p2
+        return cls(p1.x, p1.y, p2.x, p2.y)
 
-        left = min(x1, x2)
-        right = max(x1, x2)
-        bottom = min(y1, y2)
-        top = max(y1, y2)
-
-        return cls(left, bottom, right - left, top - bottom)
 
     @property
     def left(self):
-        return self._x
-
-    @property
-    def bottom(self):
-        return self._y
-
-    @property
-    def width(self):
-        return self._width
-
-    @property
-    def height(self):
-        return self._height
+        return min(self.pt1.x, self.pt2.x)
 
     @property
     def right(self):
-        return self._x + self._width
+        return max(self.pt1.x, self.pt2.x)
+
+    @property
+    def bottom(self):
+        return min(self.pt1.y, self.pt2.y)
 
     @property
     def top(self):
-        return self._y + self._height
+        return max(self.pt1.y, self.pt2.y)
 
     @property
-    def bottomleft(self):
-        return Point(self.left, self.bottom)
+    def width(self):
+        return self.right - self.left
+
+    @property
+    def height(self):
+        return self.top - self.bottom
+
 
     @property
     def topleft(self):
         return Point(self.left, self.top)
 
     @property
-    def bottomright(self):
-        return Point(self.right, self.bottom)
-
-    @property
     def topright(self):
         return Point(self.right, self.top)
 
     @property
+    def bottomleft(self):
+        return Point(self.left, self.bottom)
+
+    @property
+    def bottomright(self):
+        return Point(self.right, self.bottom)
+
+    @property
     def center(self):
         return Point(
-            self.left + self.width / 2,
-            self.bottom + self.height / 2
+            (self.left + self.right) / 2,
+            (self.bottom + self.top) / 2
         )
+
+
+    def area(self):
+        return self.width * self.height
+
+    def move(self, x, y):
+        self.pt1 = Point(self.pt1.x + x, self.pt1.y + y)
+        self.pt2 = Point(self.pt2.x + x, self.pt2.y + y)
+        return self
